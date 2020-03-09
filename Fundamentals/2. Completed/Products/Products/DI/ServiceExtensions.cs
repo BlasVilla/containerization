b74134e9c2aaef5971153487d4943f1016ccf959
Blas Villa <blasimirvilla@outlook.com>
2020-03-09 12:27:27 +0100

    Moved solutions to Fundamentals folder

---
+++ Fundamentals/2. Completed/Products/Products/DI/ServiceExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Products.Domain;
+using Products.Dummy;
+
+namespace Products.DI
+{
+    public static class ServiceExtensions
+    {
+        public static IServiceCollection AddDependencies(this IServiceCollection services)
+        {
+            return services
+                .AddSingleton<IProductRepository, DummyProductRepository>();
+        }
+    }
+}
